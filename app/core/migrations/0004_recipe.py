7768dc6df82ddbb2128b72151d9ac4f9f6ca7280
Chu-Te <ctchen002@gmail.com>
2021-04-27 10:38:24 +0800

    Added list recipe feature.

--- app/core/migrations/0004_recipe.py
+++ app/core/migrations/0004_recipe.py
@@ -17,10 +17,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
-                ('time_miniutes', models.IntegerField()),
+                ('time_minutes', models.IntegerField()),
                 ('price', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('link', models.CharField(blank=True, max_length=255)),
-                ('ingredient', models.ManyToManyField(to='core.Ingredient')),
+                ('ingredients', models.ManyToManyField(to='core.Ingredient')),
                 ('tags', models.ManyToManyField(to='core.Tag')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
